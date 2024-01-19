b072b7bc2322b2ca7d802e134bee1de621167fc8
nic-d <nicanthonidiaz@gmail.com>
2024-01-19 13:20:29 -0500

    aboutme finished

--- react-portfolio/src/components/pages/About.jsx
+++ react-portfolio/src/components/pages/About.jsx
@@ -1,13 +1,46 @@
+// npm package to import my avatar
+import Avatar from "avataaars";
+
 export default function About() {
   return (
     <div className="about">
       <h2>About Me</h2>
-      <p>WHEN I am presented with the About Me section</p>
-      <p>
-        THEN I see a recent photo or avatar of the developer and a short bio
-        about them
+
+      <Avatar
+        avatarStyle="Transparent"
+        topType="ShortHairShortCurly"
+        accessoriesType="Round"
+        hairColor="BrownDark"
+        facialHairType="MoustacheMagnum"
+        facialHairColor="BrownDark"
+        clotheType="Hoodie"
+        clotheColor="White"
+        eyeType="Default"
+        eyebrowType="Default"
+        mouthType="Smile"
+        skinColor="Light"
+      />
+      <p className="first-p">
+        My name is Nic, and I'm a 29 year old aspiring FullStack Web Developer.
+        I earned my Bachelor's degree at the University of Central Florida where
+        I studied business marketing.
       </p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est impedit quas tenetur aperiam minima? Numquam placeat fugiat ex nemo labore dicta quasi! Reiciendis perferendis totam in commodi rerum qui modi.</p>
+
+      <p>
+        I found my love for development whenever I began working as a Technical
+        Sucess Manager at a CI/CD company. As you could imagine, every customer
+        I speak with is either a developer themself, or an exectuive at their
+        organization. After having many in depth conversations with different
+        teams surrounding their tech stack and ways to optimize their build
+        processes on our platform, I quickly found how interesting and exciting
+        this whole tech world to be.
+      </p>
+
+      <p>
+        Now, over a year later with countless projects and more experience under
+        my belt, I'm ready to begin my journey as a fulltime developer. Happy
+        coding!
+      </p>
     </div>
   );
 }
