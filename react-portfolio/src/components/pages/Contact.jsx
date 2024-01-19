fd1c66f7c5cc2b76436fc7eb39c48b70cba62fc6
nic-d <nicanthonidiaz@gmail.com>
2024-01-19 12:35:20 -0500

    contact form template made

--- react-portfolio/src/components/pages/Contact.jsx
+++ react-portfolio/src/components/pages/Contact.jsx
@@ -2,25 +2,21 @@ export default function Contact() {
   return (
     <div className="contact">
       <h2>Contact Me</h2>
-      <p>WHEN I am presented with the Contact section</p>
-      <p>
-        THEN I see a contact form with fields for a name, an email address, and
-        a message
-      </p>
-      <br></br>
-      <p>
-      WHEN I move my cursor out of one of the form fields without entering text
-      </p>
-      <p>
-      THEN I receive a notification that this field is required
-      </p>
-      <br></br>
-      <p>
-      WHEN I enter text into the email address field
-      </p>
-      <p>
-      THEN I receive a notification if I have entered an invalid email address
-      </p>
+      <form>
+        <label>
+          <span>Your name:</span>
+          <input type="name" name="name" required />
+        </label>
+        <label>
+          <span>Your email:</span>
+          <input type="email" name="email" required />
+        </label>
+        <label>
+          <span>Message:</span>
+          <textarea type="text" name="message" required></textarea>
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
