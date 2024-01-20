f99fdba762ce8eb0e8a66d0d9004ab82f6b4dfa9
nic-d <nicanthonidiaz@gmail.com>
2024-01-19 20:29:20 -0500

    added styling to resume section

--- react-portfolio/src/components/pages/Resume.jsx
+++ react-portfolio/src/components/pages/Resume.jsx
@@ -10,30 +10,40 @@ export default function ResumeComponent() {
 
   return (
     <div className="resume">
-      <h2>Resume</h2>
-      <button className="download-btn" onClick={handleDownload}>
-        Download Resume
-      </button>
-      <h3>Front-end Proficiencies</h3>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Javascript</li>
-        <li>JQuery</li>
-        <li>React</li>
-        <li>Bootstrap</li>
-        <li>Tailwind</li>
-        <li>Responsive Design</li>
-      </ul>
-      <h3>Back-end Proficiencies</h3>
-      <ul>
-        <li>Node</li>
-        <li>Express</li>
-        <li>MySQL, Sequelize</li>
-        <li>MongoDB, Mongoose</li>
-        <li>REST</li>
-        <li>GraphQL</li>
-      </ul>
+      <div className="header-btn-container">
+        <h2>Resume</h2>
+        <button className="download-btn" onClick={handleDownload}>
+          Download Resume
+        </button>
+      </div>
+
+      <div className="proficiencies-container">
+        <div className="proficiencies-section">
+          <h3>Front-end Proficiencies</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>JQuery</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Tailwind</li>
+            <li>Responsive Design</li>
+          </ul>
+        </div>
+
+        <div className="proficiencies-section">
+          <h3>Back-end Proficiencies</h3>
+          <ul>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
